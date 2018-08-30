483380d2f2da0856f40366d4d452abfd540e3a4b
Aakash Nethi <aakash.nethi@parker.com>
2018-08-30 08:36:04 -0400

    Add project files.

---
+++ RFQ/Areas/MyFeature/Controllers/RFQController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RFQ.Areas.MyFeature.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RFQ.Areas.RFQPages.Controllers
+{
+    [Area("MyFeature")]
+    [Route("[area]")]
+    public class RFQController : Controller
+    {
+        private RFQModel dataModel = new RFQModel();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ RFQ/Areas/MyFeature/Models/RFQModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RFQ.Areas.MyFeature.Models
+{
+    public class RFQModel
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int Age { get; set; }
+        public decimal City { get; set; }
+
+        public RFQModel()
+        {
+            FirstName = "parker";
+            LastName = "Hannifin";
+            Age = 100;
+
+        }
+    }
+
+}
